9d2f9ec2cb100b4a633746fdc0c4e2141e2431ec
himacks <karpinsky@chapman.edu>
2021-12-05 18:16:28 -0800

    Updating files

--- datatest.py
+++ datatest.py
@@ -1,14 +1,18 @@
 from dataparser import parser
 
-nested_dict = { 'dictA': {'key_1': 'value_1'},
-                'dictB': {'key_2': 'value_2'}}
-tickerdict = { "tickers" : {"EFX" : {"Date" : { "2021-11-29" : { "flow": "1.2345", "change": "4.43"},
-                                                "2021-11-30" : { "flow": "2.2345", "change": "5.43"}
-                                              }
-                                    }
-                           }
-             }
+tickerdict = { "tickers" : {} }
 
-print(tickerdict.get("tickers").get("EFX").get("Date").get("2021-11-30").get("flow"))
+currentTicker = {"EFX" : {"Date" : {} } }
+
+tickerdict.get("tickers").update(currentTicker)
+
+testDateFlow1 = {"2021-11-29" : { "flow": "1.2345", "change": "4.43"}}
+testDateFlow2 = {"2021-11-30" : { "flow": "2.2345", "change": "5.43"}}
+testDateFlow3 = {"2021-12-01" : { "flow": "3.2345", "change": "4.43"}}
+
+
+tickerdict.get("tickers").get("EFX").get("Date").update(testDateFlow1)
+
+print(tickerdict.get("tickers").get("banana"))
 
 parser.write_file("test.json", tickerdict)
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from webull import webull
 import yfinance as yf
-from datetime import date
-import time
+from dataparser import parser
+import pprint
+
 
 
 webull = webull()
@@ -9,14 +10,20 @@ webull = webull()
 cumulativeFlow = [0] * 5
 dates = [0] * 5
 
-tickerList = ["EFX", "CDW", "ROK", "LHX", "KSU", "TT", "TRV", "WLTW", "TER", "SWK", "MSI", "VMC", "TROW", "URI", "CDNS", "FRC", "W", "SGEN", "ALB", "RNG", "TWLO", "U", "BILL"]
-
+#tickerList = ["EFX", "CDW", "ROK", "LHX", "KSU", "TT", "TRV", "WLTW", "TER", "SWK", "MSI", "VMC", "TROW", "URI", "CDNS", "FRC", "W", "SGEN", "ALB", "RNG", "TWLO", "U", "BILL"]
+tickerList = ["EFX", "CDW", "ROK", "LHX", "KSU"]
 #make this function happen whenever you initialize a new stock
 #then make a function to get the data for today and append it to the end of the
 #function. Make some cloud application run this program once a day at market close to start
 #building a data base for stocks.
 
 print()
+
+try:
+    tickerdict = parser.read_file("tickerlog.json")
+except FileNotFoundError:
+    tickerdict = { "tickers" : {} }
+
 
 for ticker in tickerList:
 
@@ -30,6 +37,10 @@ for ticker in tickerList:
     hist = symbol.history(period="1wk")
     stockFlow = [0] * 5
 
+    if(tickerdict.get("tickers").get(ticker) == None):
+        currentTicker = {ticker : {"Date" : {} }}
+        tickerdict.get("tickers").update(currentTicker)
+
     for i in range(5):
         date = flowData.get("historical")[i].get("date")
         date = date[:4] + "-" + date[4:6] + "-" + date[6:]
@@ -37,7 +48,10 @@ for ticker in tickerList:
         print(date)
         stockFlow[i] = flowData.get("historical")[i].get("item").get("majorNetFlow")
         cumulativeFlow[i] += flowData.get("historical")[i].get("item").get("majorNetFlow")
-        print("    Institution Flow (Millions): " + str(round(stockFlow[i]/1000000, 2)))
+
+        instFlow = round(stockFlow[i]/1000000, 2)
+
+        print("    Institution Flow (Millions): " + str(instFlow))
         #print("    Open: $" + str(round(float(hist.get("Open")[i]), 2)))
         #print("    Close: $" + str(round(float(hist.get("Close")[i]), 2)))
 
@@ -47,7 +61,15 @@ for ticker in tickerList:
         else:
             change = "$" + str(changeNum)
         print("    Change: " + str(change))
+
+        currDateFlow = {date : { "flow": str(instFlow), "change": str(changeNum)}}
+        tickerdict.get("tickers").get(ticker).get("Date").update(currDateFlow)
+
     print()
+
+parser.write_file("tickerlog.json", tickerdict)
+
+
 
 print("Cumulative Flow for these Stocks")
 print("If using for trading ideas, shows more consistency only when entering on reversals, not continuations")
@@ -56,6 +78,52 @@ print()
 for i in range(5):
     print(dates[i])
     print("    " + str(round(cumulativeFlow[i]/1000000, 2)))
+
+
+def updateAvgFlow(ticker):
+
+    try:
+        tickerdict = parser.read_file("tickerlog.json")
+    except FileNotFoundError:
+        print("No log exists")
+        return
+
+    if(tickerdict.get("tickers").get(ticker) == None):
+        print("Ticker doesn't exist, can't get average flow")
+        return
+
+    currentTicker = tickerdict.get("tickers").get(ticker)
+    print("doing " + ticker)
+
+    totalFlow = 0
+    totalEntries = 0
+
+    for i in range(len(currentTicker.get("Date"))):
+        dayFlow = abs(float((list(currentTicker.get("Date").items())[i][1]).get("flow")))
+        if (dayFlow > 0):
+            totalFlow += dayFlow
+            totalEntries += 1
+
+    avgFlow = {"flowAvg" : str(round((totalFlow / totalEntries), 2)) }
+    currentTicker.update(avgFlow)
+
+    parser.write_file("tickerlog.json", tickerdict)
+
+for ticker in tickerList:
+    updateAvgFlow(ticker)
+
+def displayLog():
+    try:
+        tickerdict = parser.read_file("tickerlog.json")
+    except FileNotFoundError:
+        print("No log exists")
+        return
+
+    pprint.pprint(tickerdict)
+
+displayLog()
+
+
 
 
 #print(date.fromtimestamp(time.time()))
